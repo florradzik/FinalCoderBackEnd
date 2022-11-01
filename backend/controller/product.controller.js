a9ec55e05e31f3617199154c4d806bcf4b3bf1b7
florradzik <florencia.radzik@gmail.com>
2022-11-01 20:29:00 -0300

    added product controller

---
+++ backend/controller/product.controller.js
@@ -0,0 +1,64 @@
+import ProductApi from "../api/products.api"
+
+class ProductController {
+  constructor() {
+    this.productApi = new ProductApi()
+  }
+
+  getProducts = async (req, res) => {
+    try {
+      const id = req.params.id
+      const products = await this.productApi.getProduct(id)
+      res.json(products)
+    } catch (e) {
+      console.log("Error to get products", e)
+      res.send(e)
+    }
+  }
+
+  getProductsByCategory = async (req, res) => {
+    try {
+      const category = req.params.category
+      const products = await this.productApi.getByCategory(category)
+      res.json(products)
+    } catch (e) {
+      console.log("Error to get products", e)
+      res.send(e)
+    }
+  }
+
+  insertProduct = async (req, res) => {
+    try {
+      const product = req.body
+      const saved = await this.productApi.inserProduct(product)
+      res.json(saved)
+    } catch (e) {
+      console.log("Error to save product", e)
+      res.send(e)
+    }
+  }
+
+  deleteProduct = async (req, res) => {
+    try {
+      const id = req.params.id
+      const deleted = await this.productApi.deleteById(id)
+      res.json(deleted)
+    } catch (e) {
+      console.log("Error to delete product", e)
+      res.send(e)
+    }
+  }
+
+  editProduct = async (req, res) => {
+    try {
+      const id = req.params.id
+      const edited = await this.productApi.editById(id)
+      res.json(edited)
+    } catch (e) {
+      console.log("Error to edit product", e)
+      res.send(e)
+    }
+  }
+}
+
+export default ProductController
